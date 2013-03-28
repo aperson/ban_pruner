d0c3f8980ed92fe84bb9860895085d04bcbff995
Zach McCullough <nosrepa@gmail.com>
2013-03-28 17:42:50 -0500

    rename credentials.py to config.py and added support to cache the list of unbanned users

--- ban_pruner.py
+++ ban_pruner.py
@@ -5,17 +5,32 @@ import requests
 
 
 try:
-    from credentials import *  # NOQA
+    from config import *  # NOQA
 except:
     USERNAME = 'someuser'
     PASSWORD = 'somepass'
+    CACHE = '/path/to/cache.file'
 
 
 class Bot(object):
     def __init__(self, username, password):
         self.r = praw.Reddit('/u/{} running ban_pruner.py'.format(USERNAME))
         self.r.login(username, password)
-        self.unbanned = []  # internal list of people already unbanned
+        self.unbanned = self.get_ban_list()  # internal list of people already unbanned
+
+    def get_ban_list(self):
+        '''Retrieves the unbanned from CACHE..'''
+
+        with open(CACHEFILE) as f:
+            for line in f:
+                unbanned.add(line.strip())
+            return unbanned
+
+    def set_ban_list(self):
+        '''Writes unbanned to CACHE.'''
+
+        with open(CACHEFILE, 'w') as f:
+            f.write('\n'.join(self.unbanned))
 
     def accept_mod_invites(self):
         '''Accepts moderator invites.'''
@@ -41,7 +56,7 @@ class Bot(object):
                 u = requests.get('http://reddit.com/u/{}'.format(user.name))
                 if u.status_code == 404:
                     subreddit.remove_ban(user.name)
-                    self.unbanned.append(user.name)
+                    self.unbanned.add(user.name)
                     output.append(user.name)
         return output
 
@@ -55,7 +70,8 @@ class Bot(object):
         message = (
             "I've just completed pruning your ban list, so here's a summary of what I've removed:"
             "\n\n{}\n\n   Your subreddit had a total of {} bans. {} of them were shadowbanned or "
-            "deleted and were removed from the list.  You now have {} bans.")
+            "deleted and were removed from the list.  You now have {} bans.  I have now remvoved "
+            "myself from your moderator list.  Feel free to re-add me at any time.")
         if unbanned_count == 0:
             summary = ""
         else:
@@ -70,6 +86,7 @@ class Bot(object):
             if subreddit.display_name != self.r.user.name:
                 self.process_subreddit(subreddit)
                 subreddit.remove_moderator(self.r.user.name)
+        self.set_ban_list()
 
 if __name__ == '__main__':
     bot = Bot(USERNAME, PASSWORD)
